244f5ab6d3cd8552301b8f8566026edfd392c51c
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-04-11 11:32:49 +0000

    updated deployment

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "Worlds from HCL with automation with CD Concept")
-    return "Hello {}!".format(name)
+    return "Hello & Bye {}!".format(name)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
