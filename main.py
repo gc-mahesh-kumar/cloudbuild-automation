a06823b4c63df13caa2898c7060383d5b5204ab1
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-04-21 05:42:02 +0000

    Updates

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World from Cloud Build and Cloud Deploy\n"
+  return "Hello World from Cloud Build and Cloud Deploy New Rollout- April\n"
 
 @app.route("/version")
 def version():
-  return "Helloworld 1.1\n"
+  return "Helloworld 1.2\n"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=8080)
