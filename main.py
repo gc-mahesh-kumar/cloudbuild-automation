b1fbbb9334f20605402486bee2541e17d2e947a4
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-04-21 05:34:09 +0000

    Include Cloud Deploy

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World from Cloud Build\n"
+  return "Hello World from Cloud Build and Cloud Deploy\n"
 
 @app.route("/version")
 def version():
-  return "Helloworld 1.0\n"
+  return "Helloworld 1.1\n"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=8080)
