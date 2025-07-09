185e324ec3d136ba4a02fe4d02b97441895098a1
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-07-09 09:08:50 +0000

    cloud deploy demo

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World from Cloud Build and Cloud Deploy New Rollout- April\n"
+  name = os.getenv("NAME", "World")
+  return "Hello {name} from Cloud Build and Cloud Deploy New Rollout- July\n"
 
 @app.route("/version")
 def version():
-  return "Helloworld 1.2ls
-  \n"
+  return "Helloworld\n"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=8080)
