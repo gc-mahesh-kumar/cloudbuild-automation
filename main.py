a4b24805f63cd06ff21676f6b2fa85de008874ce
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-07-10 10:04:38 +0000

    updated

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
   name = os.getenv("NAME", "World")
-  return f"<h1>Hello {name} from Cloud Build and Cloud Deploy New Rollout - July Rocking</h1>"
+  return f"<h1>Hello {name} from Cloud Build and Cloud Deploy New Rollout - July Rocking Next Release</h1>"
 
 @app.route("/version")
 def version():
